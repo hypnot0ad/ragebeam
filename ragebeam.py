bbb7f9ab23f1d3914867454a7240460fc177b645
Nick <nick@localhost.localdomain>
2017-07-02 22:38:55 +1000

    Replaced missing docopt code (TODO: remove completely)

--- ragebeam.py
+++ ragebeam.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+"""
+Usage:
+  ragebeam.py <user_name:password> [windows | linux | solaris | freebds | macos | aix ] <destination_folder>
+"""
+
 from docopt import docopt
 import sys
 import requests
